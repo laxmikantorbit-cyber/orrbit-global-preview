0fa4bca11330f5d4835b694a5ec68e20bee7c161
laxmikantorbit-cyber <laxmikant.orbit@gmail.com>
2026-09-16 20:26:16 +0530

    Route advanced CRM command centre

--- src/BusinessOS.Web/src/main.tsx
+++ src/BusinessOS.Web/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { CrmAdvancedHub } from './CrmAdvancedHub.tsx'
+
+const content = window.location.pathname === '/crm/advanced' ? <CrmAdvancedHub /> : <App />
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {content}
   </StrictMode>,
 )
